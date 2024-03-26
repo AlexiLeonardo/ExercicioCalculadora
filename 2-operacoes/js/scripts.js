ae7b3b1aaca451f9a8ce445a604c7b9fa5ca4557
Alexi Leonardo <alexi.rosas@edu.pucrs.br>
2024-03-26 16:36:35 -0300

    Primeiro commit calculadora

---
+++ 2-operacoes/js/scripts.js
@@ -0,0 +1,12 @@
+//
+
+//2-op numericas
+console.log(2+4);
+console.log(10/2);
+console.log(10*2);
+//3 - special numbers
+console.log(typeof Infinity);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(3*"a");//exemplo de Nan
